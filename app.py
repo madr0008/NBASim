b8ba25f00052aee74108b508bf3537c7b55155d7
Miguel Angel <madr0008@red.ujaen.es>
2022-12-03 19:43:49 +0100

    Interfaz hasta resultados

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from dependencias import Equipo
 
 app = Flask(__name__)
 
+
+#VARIABLES GLOBALES
+datos = list()
+
 #Ruta base: página principal
 @app.route('/', methods=['GET', 'POST'])
 def index() :
@@ -28,20 +32,18 @@ def elegir_partido() :
         #Solicitar partidos
         partidos = []
         partidos = Partido.obtenerPartidos(temporada, equipo1, equipo2)
-        print(partidos)
         return render_template('elegir_partido.html', partidos=partidos, equipo1=equipo1, equipo2=equipo2, temporada=temporada)
     
 
 #Ruta para elegir parámetros a simular
 @app.route('/simular_partido', methods=['GET', 'POST'])
 def simular_partido() :
+    global datos
     if request.method == 'POST':
         temporada = int(request.form['temporada'])
         fechaPartido = request.form['partido']
         equipo1 = request.form['equipo1'].split(",")
         equipo2 = request.form['equipo2'].split(",")
-        datos = []
-        print(fechaPartido)
         #Solicitar datos del partido
         datos = Partido.obtenerDatosPartido(equipo1, equipo2, fechaPartido, temporada)
         return render_template('simular_partido.html', datos=datos, atributos = ['JUGADOR', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', '+/-'])
@@ -49,8 +51,24 @@ def simular_partido() :
 
 #Ruta para mostrar resultados
 @app.route('/resultados', methods=['GET', 'POST'])
-def resultados() :    
-    return render_template('resultados.html')
+def resultados() :
+    global datos
+    if request.method == 'POST':
+        nCondiciones = int(request.form['nCondiciones'])
+        condiciones = []
+        for i in range(nCondiciones) :
+            condiciones.append(dict())
+            condiciones[i]["tipo"] = request.form['accion' + str(i + 1)]
+            condiciones[i]["actor"] = request.form['actor' + str(i + 1)]
+            if condiciones[i]["tipo"] == "equipo" :
+                condiciones[i]["accion"] = request.form['accionequipo' + str(i + 1)]
+            else :
+               condiciones[i]["accion"] = request.form['accionjugador' + str(i + 1)] 
+            condiciones[i]["valor"] = request.form['num' + str(i + 1)]
+            if "tirado" in condiciones[i]["accion"] :
+                condiciones[i]["tipotiro"] = request.form['tipotiro' + str(i + 1)]
+        #resultados = simularPartido(datos, condiciones)
+        return render_template('resultados.html')
 
 
 if __name__ == '__main__' :
